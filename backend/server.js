f956ef78ca8eb3369c7a757d3e45f1014c90249e
Alfred James <129067324+AlfredJames24@users.noreply.github.com>
2025-09-28 15:17:54 +0800

    Added a post function in database Day 2 Learning Web Dev

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,38 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 
-dotenv.config();
+dotenv.config(); //so we can use the .env file
 
-const app = express();
+const app = express(); //for routing
  
-app.get("/products", (req,res) => {});
+app.use(express.json()); //allows to use json data in body
 
-console.log(process.env.MONGO_URI);
+app.post("/api/products", async (req,res) => {
+    const product = req.body; //this is so we can send data
 
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message: "Please provide all fields"});
+    }
 
+    const newProduct = new Product (product)
 
-app.listen(5000, () => {
-    connectDB();
-    console.log('Server started at http://localhost:5000');
+    try {
+      await newProduct.save(); //saves input of user to db
+      res.status(201).json({ success:true, data: newProduct}); 
+    } catch (error) {
+
+        console.error("Error in creating product:", error.message);
+        res.status(500).json({success:false, message: "Server Error"});
+    }
 });
 
-//
+console.log(process.env.MONGO_URI); 
+
+app.listen(5000, () => { 
+    connectDB();
+    console.log('Server started at http://localhost:5000');
+}); //starts the server and connects to MongoDB
+
